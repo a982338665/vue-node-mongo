e7766ed768637723c1e06d579b7cbd01c58cea1f
a982338665 <982338665@qq.com>
2019-12-12 17:39:58 +0800

    多视图分块：视图命名，很少用到

--- demo4/src/App.vue
+++ demo4/src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
-<!--    //渲染一级路由的-->
+    <!--    切分页面，分成三个view，常用于，头部，尾部，中间层-->
+    <router-view class="main"></router-view>
+    <router-view class='left' name='title'></router-view>
+    <router-view class="right" name='img'></router-view>
+    <!--    //渲染一级路由的-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App'
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .left,.right{
+    float: left;
+    width:49%;
+    border:1px solid gray ;
+  }
 </style>
--- demo4/src/router/index.js
+++ demo4/src/router/index.js
@@ -11,7 +11,7 @@ Vue.use(Router)
 export default new Router({
   //当配置为history时 http://localhost:8080/goods/id访问
   //当去掉该配置，默认hash，则使用http://localhost:8080/#/goods/id访问
-  mode:'history',//'hash'
+  mode: 'history',//'hash'
   routes: [
     {
       path: '/goods/:goodsId',//通过路由 http://localhost:8080/#/goods/id访问 【#默认指Hash，可以通过mode：'history'设置给去掉】
@@ -22,16 +22,16 @@ export default new Router({
       path: '/goods2',
       name: 'GoodsList2',
       component: GoodsList2,
-      children:[//定义子路由
+      children: [//定义子路由
         {
-          path:'title',
-          name:'title',
-          component:Title
+          path: 'title',
+          name: 'title',
+          component: Title
         },
         {
-          path:'image',
-          name:'image',
-          component:Image
+          path: 'image',
+          name: 'image',
+          component: Image
         }
       ]
     },
@@ -40,6 +40,14 @@ export default new Router({
       // path: '/cart/:id',
       name: 'cart',
       component: Cart,
-    }
-  ]
+    },
+    {
+      path: '/',//命名视图
+      name: 'GoodsList',
+      components:{
+        default:GoodsList,
+        title:Title,
+        img:Image
+      },
+    }]
 })
