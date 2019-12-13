282264c613a8d8f1903c25c2955539b92a42fffa
a982338665 <982338665@qq.com>
2019-12-13 20:11:14 +0800

    dev-商品列表页改造

---
+++ simple-mall/src/components/NavBread.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--  此处必须有外部元素包裹它-->
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+<!--        <span>Goods</span>-->
+<!--        插槽-->
+        <slot name="bread1"></slot>
+<!--        <slot name="bread2"></slot>-->
+      </nav>
+    </div>
+  </div>
+
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
